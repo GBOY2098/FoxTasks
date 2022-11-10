a28210905100bb33f938c250027ceabe59f523c4
some0person <109872677+some0person@users.noreply.github.com>
2022-11-10 12:10:21 +0300

    "work" page is working now!!

--- BACK/main.py
+++ BACK/main.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='../FRONT', static_folder='../FRONT/CSS')
+
+
+@app.route("/work")
+def work():
+    return render_template('work.html')
 
 
 @app.route("/")
 def home():
-    return "Привет"
+    return render_template('main.html')
 
 
 if __name__ == '__main__':
