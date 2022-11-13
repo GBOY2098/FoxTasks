717b5a731ab166a91edbe2c4235ab6ab323137b4
Nor1gami <olkhovik.p@gmail.com>
2022-11-13 23:45:30 +0300

    Moved answers to tasks

--- BACK/main.py
+++ BACK/main.py
@@ -1,21 +1,21 @@
 from flask import Flask, render_template, request
 
 app = Flask(__name__, template_folder='../FRONT', static_folder='../FRONT/STATIC')
-tasks = [{'photo' :'/STATIC/Tasks/Picsart_22-11-10_19-58-45-176.jpg', 'id' : 0, 'last_answer' : '', 'is_correct': None},
-         {'photo' :'/STATIC/Tasks/Picsart_22-11-10_19-59-41-244.jpg', 'id' : 1, 'last_answer' : '', 'is_correct': None},
-         {'photo' :'/STATIC/Tasks/Picsart_22-11-10_20-00-36-549.jpg', 'id' : 2, 'last_answer' : '', 'is_correct': None},
-         {'photo' :'/STATIC/Tasks/Picsart_22-11-10_20-01-03-923.jpg', 'id' : 3, 'last_answer' : '', 'is_correct': None}]
+tasks = [{'photo' :'/STATIC/Tasks/Picsart_22-11-10_19-58-45-176.jpg', 'id' : 0, 'answer': 1 ,'last_answer' : '', 'is_correct': None},
+         {'photo' :'/STATIC/Tasks/Picsart_22-11-10_19-59-41-244.jpg', 'id' : 1, 'answer': 2 ,'last_answer' : '', 'is_correct': None},
+         {'photo' :'/STATIC/Tasks/Picsart_22-11-10_20-00-36-549.jpg', 'id' : 2, 'answer': 3 ,'last_answer' : '', 'is_correct': None},
+         {'photo' :'/STATIC/Tasks/Picsart_22-11-10_20-01-03-923.jpg', 'id' : 3, 'answer': 4 ,'last_answer' : '', 'is_correct': None}]
 
-answers = ['0', '1', '2', '3']
+
 
 @app.route("/work", methods=['POST', 'GET'])
 def work():
     if request.method == 'POST':
         id = list(request.form.keys())[0]
         answer = request.form[str(id)]
-        if answer == answers[int(id)] and tasks[int(id)]['last_answer']=="":
+        if answer == str(tasks[int(id)]['answer']) and tasks[int(id)]['last_answer']=="":
             tasks[int(id)]['is_correct'] = 0
-        elif answer == answers[int(id)]:
+        elif answer == str(tasks[int(id)]['answer']):
             tasks[int(id)]['is_correct'] = 2
         else:
             tasks[int(id)]['is_correct'] = 1
