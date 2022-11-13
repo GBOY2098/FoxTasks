4d5c05061db104eac39f628887bad5b1ef4f55c8
Nor1gami <olkhovik.p@gmail.com>
2022-11-13 03:09:53 +0300

    Добавил проверку для первого задания (убогую)
    Надо подумать как реализовать одновременную проверку всех заданий

--- FRONT/js/script.js
+++ FRONT/js/script.js
@@ -2,12 +2,10 @@ var src1 = "Tasks/Picsart_22-11-10_19-58-45-176.jpg"
 var src2 = "Tasks/Picsart_22-11-10_19-59-41-244.jpg"
 var src3 = "Tasks/Picsart_22-11-10_20-00-36-549.jpg"
 var src4 = "Tasks/Picsart_22-11-10_20-01-03-923.jpg"
-// const form =document.querySelector(".task__form")
-// const answer = document.querySelector(".task__answer")
 const list_el =document.querySelector(".tasks")
 function addTask(src,task_number){
   const task_el = document.createElement("article")
-  task_el.classList.add("task")
+  task_el.classList.add("task"+String(task_number))
   const task_form_el = document.createElement("form")
   task_form_el.classList.add("task__form"+String(task_number))
   task_form_el.method ="get"
@@ -31,6 +29,22 @@ function addTask(src,task_number){
 for(var i=1; i<5;i++){
   addTask(eval("src"+String(i)),i)
 }
+const form =document.querySelector(".task__form1")
+var correct_answer = "10"
+form.addEventListener('submit', function (event) {
+  event.preventDefault()
+  const answer = document.querySelector(".task__answer1")
+  const task_el = document.querySelector(".task1")
+  if (answer.value==correct_answer){
+    const answer_correction_el=document.createElement("div")
+    answer_correction_el.classList.add("true","container")
+    task_el.appendChild(answer_correction_el)
+  }else{
+    const answer_correction_el=document.createElement("div")
+    answer_correction_el.classList.add("false","container")
+    task_el.appendChild(answer_correction_el)
+  }
+})
 
 
 
