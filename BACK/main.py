d033b4261db954b0595b9affee9976a3b3ecf42b
heatungie <67313157+heatungie@users.noreply.github.com>
2022-11-14 23:29:04 +0300

    Add works groups

--- BACK/main.py
+++ BACK/main.py
@@ -5,7 +5,11 @@ tasks = [{'photo': '/STATIC/Tasks/Picsart_22-11-10_19-58-45-176.jpg', 'id': 0, '
          {'photo': '/STATIC/Tasks/Picsart_22-11-10_19-59-41-244.jpg', 'id': 1, 'answer': 1 ,'last_answer': '', 'is_correct': None},
          {'photo': '/STATIC/Tasks/Picsart_22-11-10_20-00-36-549.jpg', 'id': 2, 'answer': 2 ,'last_answer': '', 'is_correct': None},
          {'photo': '/STATIC/Tasks/Picsart_22-11-10_20-01-03-923.jpg', 'id': 3, 'answer': 3 ,'last_answer': '', 'is_correct': None},]
-stats = [0, 0, len(tasks)]
+
+stats = {'completed': True, 
+         'started': True,
+         'correct': 0, 
+         'answers': len(tasks)}
 
 
 
@@ -14,22 +18,19 @@ def work():
     if request.method == 'POST':
         id = list(request.form.keys())[0]
         answer = request.form[str(id)]
-        if answer == str(tasks[int(id)]['answer']) and tasks[int(id)]['last_answer']=="":
-            tasks[int(id)]['is_correct'] = 0
-            stats[1]+=1
+        if answer == str(tasks[int(id)]['answer']) and not tasks[int(id)]['last_answer']:
+            tasks[int(id)]['is_correct'] = 1
+            stats['correct']+=1
         elif answer == str(tasks[int(id)]['answer']):
             tasks[int(id)]['is_correct'] = 2
         else:
-            tasks[int(id)]['is_correct'] = 1
+            tasks[int(id)]['is_correct'] = 0
         tasks[int(id)]['last_answer'] = answer
     return render_template('work.html', tasks=tasks)
 
 @app.route("/")
 def home():
     return render_template('index.html', stats=stats)
-@app.route("/complited")
-def complited():
-    return render_template('complited.html', stats=stats)
 
 
 if __name__ == '__main__':
